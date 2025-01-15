18314f27b40a7383fb87392a97ffb6d219447f13
erichung94 <47572206+erichung94@users.noreply.github.com>
2025-01-14 22:10:59 -0600

    Adding in firebase authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,29 @@
-import  { signInWithGooglePopup, createUserDocumentFromAuth }  from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import  { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect }  from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+    useEffect(()=> {
+        const response = getRedirectResult(auth);
+        console.log(response);
+    }, [])
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     };
+
+    
     return (
         <div>
             <h1>Sign in page</h1>
             <button onClick={logGoogleUser}>
-                Sign in with Google Popup</button>
+                Sign in with Google Popup
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
+            </button>
         </div>
     )
 }
